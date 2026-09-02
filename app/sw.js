42453d085c2eff5da312fe149636b1197a26d6ad
Riden4649 <hide@airvest.net>
2026-09-02 10:26:46 +0900

    Refresh cache for integrated UI v1.9.5 vehicle-first layout

--- app/sw.js
+++ app/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "integrated-price-navi-ver1-9-4-tire-style-ui-r2";
+const CACHE_NAME = "integrated-price-navi-ver1-9-5-vehicle-first-ui-r1";
 const APP_SHELL = [
   "./",
   "./index.html",
